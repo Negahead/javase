283bffe2ac8940232835babcfc3874a94ec90279
will <hahaiamzhouwei@gmail.com>
2018-03-18 14:55:02 +0800

    init

--- src/languagetopic/ClassLoaderTest.java
+++ src/languagetopic/ClassLoaderTest.java
@@ -80,11 +80,12 @@ public class ClassLoaderTest {
         // the user defined ClassLoader is loaded by the AppClassLoader(System ClassLoader)
         System.out.println(classLoader.getClass().getClassLoader());
         try {
-            Class<?> string = classLoader.loadClass("String");
-            Object o1 = string.newInstance();
-            Method greetingMethod = string.getDeclaredMethod("greeting");
-            greetingMethod.invoke(o1);
-            Class<?> hello = classLoader.loadClass("Hello"); // load the class,will not trigger the static block.
+
+            // MyClassLoader.class is determined at compile-time.but Class.forName() will.
+            Class<?> hello = classLoader.loadClass("home.me.Hello"); // load the class,will not trigger the static block.
+            Object o1 = hello.newInstance();// return the specific class object
+            Method greetingMethod = hello.getDeclaredMethod("greeting",String.class);
+            greetingMethod.invoke(o1,"dopa");
             //Java compiler has no idea of the type you're creating when you call newInstance.so you only
             // get an Object back instead of a Hello object.
             // but Class<String> cls = Class.forName("java.lang.String");
@@ -134,6 +135,7 @@ class MyClassLoader extends ClassLoader {
     public MyClassLoader() {
         super(MyClassLoader.class.getClassLoader());
     }
+
     private Class getClass(String name) throws ClassNotFoundException {
         String file  = name.replace(".", File.separator) + ".class";
         byte[] b = null;
@@ -156,7 +158,7 @@ class MyClassLoader extends ClassLoader {
     }
 
     private byte[] loadClassFileData(String name) throws IOException {
-        System.out.println("the current class's class loader is " + getClass().getClassLoader());
+        System.out.println("the current class's class loader is " + getClass().getClassLoader()); // system classloader
         InputStream stream = getClass().getClassLoader().getResourceAsStream(name);
         int size = stream.available();
         byte buff[] = new byte[size];
@@ -177,3 +179,177 @@ class MyClassLoader extends ClassLoader {
 
 
 }
+
+/**
+ * the full story of object initialization in Java
+ *
+ *      An object is a chunk of memory bundled with the code that manipulates memory.In the memory,the object
+ *      maintains its state(the value of its instance memory),which can change and evolve throughout its lifetime.
+ *      to get a newly created object off to a good start.its newly-allocated memory must be initialized to a
+ *      proper initial state.
+ *
+ *      At the beginning of an object's life,the JVM allocates enough memory on the heap to accommodate the object's
+ *      instance variables.When that memory is first allocated,however,the data it contains is unpredictable.If
+ *      the memory were used as is,the behavior of the object would also be unpredictable.To guard against such
+ *      a scenario,Java makes certain that memory is initialized,at least to predictable default values,before
+ *      it is used by any code.
+ *
+ *      The Java language has three mechanism dedicated to ensure proper initialization of objects:
+ *          -- instance initialization blocks
+ *          -- instance variable initializer
+ *          -- constructors
+ *      when you allocate memory for a new object with the new operator,the JVM will insure that initialization
+ *      code is run before you can use the newly-allocated memory.if you provide no explicit initialization to
+ *      instance variables,they will be awarded predictable default initial values,which are based only on the
+ *      type of the variable.if you don't explicitly initialize an instance variable,that variable will retain
+ *      its default initial value when new returns its object reference.
+ *
+ *      The central player in object initialization is the constructor.constructors are noe inherited by subclasses.
+ *
+ *      When you compile a class,the Java compiler creates an instance initialization method for each constructors
+ *      you declare in the source code of the class.Although the constructor is not a method,the instance
+ *      initialization method is.It has a name,<init> ,a return type,void.and a set of parameters that match the
+ *      parameters of the constructors from which it was generated.look at the following class:
+ *              class CoffeeCup {
+ *                  public CoffeeCup() {}
+ *                  public CoffeeCup(int amount) {}
+ *              }
+ *      the compiler would generate that following two instance initialization methods in the class file for class
+ *      CoffeeCup,one for each constructors in the source file:
+ *              public void <init>(CoffeeCup this) {}   (initialization method)
+ *              public void <init>(CoffeeCup this,int amount) {}
+ *      this reference will passed into the parameter list of every instance method.for example,the instance
+ *      method void add(int amount) in the source file for class CoffeeCup would become the
+ *      void add(CoffeeCup this,int amount) method in the class file.the hidden this reference is the way in
+ *      which instance methods,including instance initialization methods,are able to access instance data.
+ *
+ *      If you don't explicitly declare a constructor in a class,the Java compiler will create a default
+ *      constructor on the fly,then translate that default constructor into a corresponding instance
+ *      initialization method.Thus every class will have at least one instance initialization method.
+ *
+ *      The <init> method is not actually part of the Java language,Rather,it is something the JVM expects
+ *      to see in a Java class file.
+ *
+ *      instance initialization block:
+ *          -- initializer code must catch exceptions
+ *          -- performs fancy calculations that can't be expressed with an instance variable initializer.
+ *      also useful in anonymous inner classes,which can not declare any constructors at all.
+ *
+ *      instance variable initializer(private int age = 23)
+ *      instance initializer(initializer block)
+ *
+ *      YOU CAN'T MAKE A FORWARD REFERENCE FROM AN INITIALIZER.
+ *
+ *
+ *
+ *      The structure of <init>:
+ *          in <init> method,the compiler can place three kinds of code:
+ *              -- An invocation of another constructor
+ *                      this();
+ *                      For any class except class java.lang.Object,if you write a constructor that does
+ *                      not begin with a this() invocation,the <init> method for that constructor will
+ *                      begin with an invocation of a superclass constructor.(Note that you can't have
+ *                      both this() and super() in the same constructor)
+ *
+ *                      One other rule enforced on constructors is that you can't catch any exceptions
+ *                      thrown by the constructor invoked with this() or super(),the proper way to signal
+ *                      that an error occurred during object initialization is by throwing an exception.
+ *              -- Instance variable initializer
+ *                      instance variables are initialized in an order starting from class Object and proceeding
+ *                      down the inheritance path to the object's class
+ *              -- The constructor body
+ *          The order in which the compiler places these components into the <init> method determines the order
+ *          of initialization of an object's fields.
+ */
+
+class Liquid {
+    private int mlVolume;
+    private float temperature;
+    Liquid(int mlVolume,float temperature) {
+        this.mlVolume = mlVolume;
+        this.temperature = temperature;
+    }
+}
+
+/**
+ * When you instantiate a new Coffee object with the new operator,the Java virtual machine first will allocate enough
+ * space on the heap to hold all the instance variables declared in Coffee and its superclasses.Second,the virtual
+ * machine will initialize all the instance variables to their default initial values,Third,the virtual machine will
+ * invoke the <init> method in the Coffee class.
+ *
+ * the first thing Coffee's <init> method will do is invoke the <init> method in its direct superclass,Liquid,the first
+ * thing Liquid's <init> method will do is invoke the no-arg <init> method in its direct superclass,Object,Object's
+ * <init> method most likely will do nothing but return.When Object's <init> method returns, Liquids <init> method will
+ * initialize mlVolume and temperature to their proper starting values and return. When Liquids <init> method returns,
+ * Coffee's <init> method will initialize swirling and clockwise to their proper starting values and return. Upon normal
+ * completion of Coffee's <init> method (in other words, so long as it doesn't complete abruptly by throwing an exception),
+ * the JVM will return the reference to the new Coffee object as the result of the new operator.
+ */
+class Coffee extends Liquid {
+    private boolean swirling;
+    private boolean clockwise;
+
+    public Coffee(int mlVolume,float temperature,boolean swirling,boolean clockwise) {
+        super(mlVolume,temperature);
+        this.swirling = swirling;
+        this.clockwise = clockwise;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
